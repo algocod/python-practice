c436eb6b999058f506b6cbdc31c3998e8e6ab990
appuhem <usmailhk@gmail.com>
2022-06-10 17:08:29 -0400

    basic function testing

--- scope/scopechk.py
+++ scope/scopechk.py
@@ -4,3 +4,8 @@ if num1 < num2:
     print('level 1 ')
     num3 = 6
 print(num3)# scope of num3 outlives the if condition . This sounds like its top to bottom as in top code cant access yet to be created variables in bottom but bottom code can access the variables earlier in the top
+
+if num1 > num2:
+    newvar = 'not reachable' # the code has to be reachable during execution to create the variable hence best way is to NOT use the variables within loop down the line
+
+#print(newvar)#NameError: name 'newvar' is not defined
